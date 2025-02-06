d579b0fd90751a0ff52857ee9d06902c111073b2
Subhadeep-Samanta <subhadeepsamanta1535@gmail.com>
2025-02-06 10:03:11 +0530

    Milestone_5 update

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import {AiOutlineEye,AiOutlineEyeInvisible} from "react-icons/ai"
-import styles from "../styles/styles.js"
+import styles from "../../styles/styles.js"
 import { Link } from "react-router-dom";
 
 const Login = () => {
